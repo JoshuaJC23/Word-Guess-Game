f7091a487f1d9e4d2e908643683d60527d766b24
Ball Man <joshua.christensen13@gmail.com>
2021-11-16 14:16:25 -0700

    added the split method and used _ to store the space of the letters that will change once key is pressed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,17 +10,31 @@ var wins = 0;
 var losses = 0;
 var guessesRemaining = 15;
 var guessesSoFar = "";
-var currentWord;
+// var currentWord;
 var NumberofBlanks = [];
-var lettersInWord = [];
+// var lettersInWord = [];
 var team;
-
+var randTeam;
 // for(var i = 0; i < teams.length; i++) {
 //     console.log(teams[i]);
 // };
+    function random() {
+        randTeam = teams[Math.floor(Math.random() * teams.length)];
+        console.log(randTeam);
+        // randNum.toLowerCase();
+        team = teams[randTeam];
+        
+    _teamName = document.getElementById("CurrentWord");
+    _teamName.textContent = randTeam.split("");
+    }
 
-var randNum = teams[Math.floor(Math.random() * teams.length)];
-console.log(randNum);
+random();
 
-_teamName = document.getElementById("CurrentWord");
-_teamName.textContent = randNum.split("");
+function blanks() {
+    NumberofBlanks = [];
+    for(var i = 0; i < randTeam.length; i++){
+        NumberofBlanks[i] = "_";
+    }
+    document.getElementById("CurrentWord").textContent = NumberofBlanks.join(" ")
+}
+blanks();
