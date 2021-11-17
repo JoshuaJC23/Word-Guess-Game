83d9c8c887df4add400b50352d9cef1735efac66
Ball Man <joshua.christensen13@gmail.com>
2021-11-17 14:05:35 -0700

    got the correct and incorrect letters to show in the already guessed element. Also got the correct letters to exchange for the _.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,18 @@
 // create variable with array of nba teams
 var teams = ["Celtics", "Bulls", "Hawks", "Nets", "Cavaliers", "Hornets", "Knicks",
-                "Pistons", "Heat", "SeventySixers", "Pacers", "Magic", "Raptors", "Bucks",
+                "Pistons", "Heat", "Seventy Sixers", "Pacers", "Magic", "Raptors", "Bucks",
                 "Wizards", "Nuggets", "Warriors", "Mavericks", "Timberwolves", "Thunder",
                 "Blazers", "Jazz", "Clippers", "Lakers", "Suns", "Kings", "Rockets", "Grizzlies",
                 "Pelicans", "Spurs"];
-
+ 
+// var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+//                 "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // create variables
 var wins = 0;
 var losses = 0;
 var guessesRemaining = 15;
-var guessesSoFar = "";
-// var currentWord;
+var guessesSoFar = [];
+var userGuess = "";
 var NumberofBlanks = [];
 // var lettersInWord = [];
 var team;
@@ -22,7 +24,7 @@ var randTeam;
         randTeam = teams[Math.floor(Math.random() * teams.length)];
         console.log(randTeam);
         // randNum.toLowerCase();
-        team = teams[randTeam];
+        // team = teams[randTeam];
         
     _teamName = document.getElementById("CurrentWord");
     _teamName.textContent = randTeam.split("");
@@ -35,6 +37,50 @@ function blanks() {
     for(var i = 0; i < randTeam.length; i++){
         NumberofBlanks[i] = "_";
     }
-    document.getElementById("CurrentWord").textContent = NumberofBlanks.join(" ")
+    document.getElementById("CurrentWord").textContent = NumberofBlanks.join(" ");
 }
 blanks();
+
+console.log(randTeam.length);
+
+var lettersGuessed = document.getElementById("LettersAlreadyGuessed");
+var numberOfGuesses = document.getElementById("NumberOfGuessesRemaining");
+numberOfGuesses.textContent = guessesRemaining;
+var fillBlanks = document.getElementById("CurrentWord")
+
+document.onkeyup = (function(event){
+    userGuess = event.key.toLowerCase();
+    console.log(userGuess);
+
+   if(randTeam.indexOf(userGuess) !== -1){
+    for(var i = 0; i < randTeam.length; i++){
+        if(randTeam[i] === userGuess){
+            NumberofBlanks[i] = userGuess;
+        } 
+    }
+   }
+   if(randTeam[i] !== userGuess){
+    guessesSoFar.push(userGuess);
+}
+   fillBlanks.textContent = NumberofBlanks.join(" ");
+
+    // else{
+    //     for(var i = 0; i < randTeam.length; i++){
+    //         if(randTeam)
+    //     }
+    // }
+    lettersGuessed.textContent = guessesSoFar;
+});
+    // checkLetter();
+
+
+// function checkLetter(){
+//     if(guessesSoFar.indexOf(randTeam) === -1){
+//         guessesSoFar.push(randTeam);
+//         if(randTeam.indexOf(randTeam) === -1){
+//             numberOfGuesses--;
+//         }
+//     }
+
+// }
+// checkLetter();
